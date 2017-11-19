956161595299e11291216cc7e6a15fb9b98704db
nx111 <gd.zhangdz@gmail.com>
2017-11-19 23:36:19 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -14,10 +14,6 @@ export CCACHE_DIR=$workdir/.ccache
 $workdir/prebuilts/misc/linux-x86/ccache/ccache -M 30G
 
 export JACK_SERVER_VM_ARGUMENTS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx4096m"
-if [ -x $workdir/out/host/linux-x86/bin/jack-admin ]; then
-   $workdir/out/host/linux-x86/bin/jack-admin kill-server
-   $workdir/out/host/linux-x86/bin/jack-admin start-server
-fi
 
 . $workdir/build/envsetup.sh
 
@@ -28,12 +24,18 @@ if [ $# -ge 1 -a "$1" = "addonsu" ]; then
 	[ -f $workdir/.mypatches/superuser.rc -a ! -f $workdir/system/extras/su/superuser.rc ] \
 		&& cp $workdir/.mypatches/superuser.rc $workdir/system/extras/su/
 	make addonsu
+elif [ $# -ge 1 -a "$1" = "boot" ]; then
+	breakfast kltechnduo
+	make -B bootimage
+#elif [ $# -ge 1 -a "$1" = "multirom" ]; then
+#	breakfast kltechnduo
+#	make multirom_zip
 else
 
 	rm -rf $workdir/out/target/product/kltechnduo/system
 	rm -rf $workdir/out/target/product/kltechnduo/root
 	rm -rf $workdir/out/target/product/kltechnduo/lineage_kltechnduo-ota-*.zip
-
+	rm -rf $workdir/out/target/product/kltechnduo/obj/PACKAGING/*
 	brunch kltechnduo
 fi
 
