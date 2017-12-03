c2acf40e16716c8ad90cd2dc0f9614a66c0daf1d
nx111 <gd.zhangdz@gmail.com>
2017-12-03 21:17:37 +0800

    skip some changes.

--- repopick.sh
+++ repopick.sh
@@ -3,15 +3,15 @@
 source build/envsetup.sh
 
 # vendor/lineage
-repopick 188834 # repopick: Support overriding the default commits count to check
+#repopick 188834 # repopick: Support overriding the default commits count to check
 
 # build/make
-repopick 190430 # Allow setting the recovery density separately from the aapt config
+#repopick 190430 # Allow setting the recovery density separately from the aapt config
 
 
 # hardware/qcom/gps
-repopick 185676 # Revert "msm8974: remove from top level makefile"
-repopick 185675 # Revert "msm8974: deprecate msm8974"
+#repopick 185676 # Revert "msm8974: remove from top level makefile"
+#repopick 185675 # Revert "msm8974: deprecate msm8974"
 
 # hardware/qcom/media
 #repopick 185806 # mm-video: venc: Correct a typo in variable name
@@ -21,44 +21,44 @@ repopick 185675 # Revert "msm8974: deprecate msm8974"
 
 # Recovery updates
 repopick 186687; ## https://review.lineageos.org/#/c/186687
-repopick 187332 187374; ## https://review.lineageos.org/#/q/change:187332+OR+change:187374
-repopick 187155; ## https://review.lineageos.org/#/c/187155
+#repopick 187332 187374; ## https://review.lineageos.org/#/q/change:187332+OR+change:187374
+#repopick 187155; ## https://review.lineageos.org/#/c/187155
 
 # Bionic and native
-repopick -f 185639 # Restore android_alarm.h kernel uapi header
+#repopick -f 185639 # Restore android_alarm.h kernel uapi header
 repopick 185640; ## https://review.lineageos.org/#/c/185640  #Add support for dynamic SHIM libraries 
-repopick 185671; ## https://review.lineageos.org/#/c/185671  #native: Increase ART heap limit to 192MB for 1024MB RAM devices 
-repopick 190614 # linker: allow the linker to shim executables
+#repopick 185671; ## https://review.lineageos.org/#/c/185671  #native: Increase ART heap limit to 192MB for 1024MB RAM devices 
+#repopick 190614 # linker: allow the linker to shim executables
 
 # Apps updates
 #repopick 188389 188518-188526 191605; ## https://review.lineageos.org/#/q/project:LineageOS/android_packages_apps_Camera2+branch:lineage-15.0
-repopick 188527-188529; ## https://review.lineageos.org/#/q/project:LineageOS/android_packages_apps_Gallery2+branch:lineage-15.0
+#repopick 188527-188529; ## https://review.lineageos.org/#/q/project:LineageOS/android_packages_apps_Gallery2+branch:lineage-15.0
 #repopick 186688; ## https://review.lineageos.org/#/c/186688      #FMRadio: jni: Add missing liblog dependency
 
 # Legacy ADB
 repopick -t oreo-adb-usb-legacy; ## https://review.lineageos.org/#/q/oreo-adb-usb-legacy
 
 # System core updates
-repopick 185888; ## utils: Threads: Handle empty thread names  #https://review.lineageos.org/#/c/185888
-repopick 187146; ## init: don't reboot to bootloader on panic  #https://review.lineageos.org/#/q/change:187146   
+#repopick 185888; ## utils: Threads: Handle empty thread names  #https://review.lineageos.org/#/c/185888
+#repopick 187146; ## init: don't reboot to bootloader on panic  #https://review.lineageos.org/#/q/change:187146   
 
 # UI updates
-repopick 193830; ## https://review.lineageos.org/#/q/change:193830
+#repopick 193830; ## https://review.lineageos.org/#/q/change:193830
 ## repopick 194226; ## https://review.lineageos.org/#/q/change:194226 ,this changes will cause file explorer crash.
 
 # Topic: android-o-camera-hal1 (188388 temporary)
-repopick -Q 'status:open+topic:android-o-camera-hal1';  ## https://review.lineageos.org/#/q/status:open+topic:android-o-camera-hal1
-repopick -f 188388; ## https://review.lineageos.org/#/c/188388
+#repopick -Q 'status:open+topic:android-o-camera-hal1';  ## https://review.lineageos.org/#/q/status:open+topic:android-o-camera-hal1
+#repopick -f 188388; ## https://review.lineageos.org/#/c/188388
 
 # Topic: LineageOS Features
-repopick 193540; ## https://review.lineageos.org/#/c/193540 ,  #  Apply LineageOS rebrand
+#repopick 193540; ## https://review.lineageos.org/#/c/193540 ,  #  Apply LineageOS rebrand
 #repopick 191921; ## https://review.lineageos.org/#/q/change:191921 ,# settings: Kill night mode if we have LiveDisplay
 repopick 187592; ## https://review.lineageos.org/#/q/change:187592 ,# settings: Add developer setting for root access
-repopick 191942 193188 193812 192510 191922 192516 192517 ; ## https://review.lineageos.org/#/q/status:open+topic:oreo-powermenu
+repopick -Q 'status:open+topic:oreo-powermenu' ; ## https://review.lineageos.org/#/q/status:open+topic:oreo-powermenu
 #repopick 193249 193258;  ## https://review.lineageos.org/#/q/change:193249+OR+change:193258
-repopick 191905; ## https://review.lineageos.org/#/q/change:191905
-repopick 193025 191736; ## https://review.lineageos.org/#/q/change:193025+OR+change:191736
-repopick -Q 'status:open+topic:oreo-tiles'; ## https://review.lineageos.org/#/q/status:open+topic:oreo-tiles
+#repopick 191905; ## https://review.lineageos.org/#/q/change:191905
+#repopick 193025 191736; ## https://review.lineageos.org/#/q/change:193025+OR+change:191736
+#repopick -Q 'status:open+topic:oreo-tiles'; ## https://review.lineageos.org/#/q/status:open+topic:oreo-tiles
 #repopick 193544 192490 193490 193770; ## https://review.lineageos.org/#/q/change:193544+OR+change:192490+OR+change:193490+OR+change:193770
 
 # Topic: LineageOS Lights
@@ -71,9 +71,9 @@ repopick -Q 'status:open+topic:oreo-tiles'; ## https://review.lineageos.org/#/q/
 
 #repopick 195736 ## Move QCOM mm codecs to vendor partition
 #repopick 195731 ## Move QCOM HALs to vendor partition
-repopick 195655 ## macloader: Move device dependent modules to /vendor
-repopick 195649  ## liblights: Move device dependent modules to /vendor
-repopick 195648 ## consumerir: Move device dependent modules to /vendor
+#repopick 195655 ## macloader: Move device dependent modules to /vendor
+#repopick 195649  ## liblights: Move device dependent modules to /vendor
+#repopick 195648 ## consumerir: Move device dependent modules to /vendor
 
 
 ########### my patches ####################
