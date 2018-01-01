036b4c523927925b617d5f547444c2a31cba4acc
nx111 <gd.zhangdz@gmail.com>
2018-01-01 21:27:12 +0800

    repopick.sh: support snap and restore project.

--- repopick.sh
+++ repopick.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 source build/envsetup.sh
-branch_reset=0
-patch_diff=0
-save_patch=0
+op_reset_projects=0
+op_patch_local=0
+op_snap_projects=0
+op_restore_snap=0
 
 ########## main ###################
 
 for op in $*; do
-    [ "$op" = "-p" ] && patch_diff=1
-    [ "$op" = "-r" ] && branch_reset=1
-    [ "$op" = "-s" ] && save_patch=1
+    [ "$op" = "-pl " -o "$op" = "--patch_local" ] && op_patch_local=1
+    [ "$op" = "--reset" -o "$op" = "-r" ] && op_reset_projects=1
+    [ "$op" = "--snap" -o "$op" = "-s" ] && op_snap_projects=1
+    [ "$op" = "--restor" -o "$op" = "--restore-snap" ] && op_restore_snap=1
     if [ "$op" = "-rp" -o "$op" = "-pr" ]; then
-        patch_diff=1
-        branch_reset=1
+        op_patch_local=1
+        op_reset_projects=1
     fi
 done
 
 ##### apply patch saved first ########
-function patch_saved()
+function patch_local()
 {
     cd $(gettop)
     topdir=$(gettop)
@@ -51,7 +53,8 @@ function patch_saved()
     done
     cd $topdir
 }
-function reset_branches()
+
+function projects_reset()
 {
     cd $(gettop)
     topdir=$(gettop)
@@ -62,8 +65,6 @@ function reset_branches()
          if ! grep -q "^$project\$" $topdir/.repo/project.list; then
               continue
          fi
-         rm -rf $topdir/.mypatches/$project/*
-
          cd $topdir/$project
          echo ""
          echo "==== reset $project to $basebranch "
@@ -74,82 +75,206 @@ function reset_branches()
     cd $topdir
 }
 
-function save_patches()
+function projects_snap()
 {
     cd $(gettop)
     topdir=$(gettop)
     default_branch=$(cat .repo/manifest.xml | grep "default revision" | cut -d= -f2 | sed -e "s/\"//g" -e "s/refs\/heads\///")
-
-    find .mypatches -type d | sed -e "s/\.mypatches\///" |sort -n | while read project; do
-         [ "$f" = ".mypatches" ] && continue
-         if ! grep -q "^$project\$" $topdir/.repo/project.list; then
-              continue
-         fi
-         rm -rf $topdir/.mypatches/$project/*
-
+    snap_file=$topdir/.mypatches/projects_snap.list
+    rm -f $snap_file.new
+    cat $topdir/.repo/project.list | while read project; do
          cd $topdir/$project
-         echo ""
-         echo "==== save patches for $project: "
+         basebranch=$(git branch -a | grep '\->' | grep "$default_branch" | sed -e "s/.*\-> //")
+         basecommit=$(git log --pretty=short -1 $basebranch | sed -n 1p | cut -d' ' -f2)
+         echo "$project, $basebranch, $basecommit" >> $snap_file.new
+
+         [ -d $topdir/.mypatches/$project ] || mkdir -p $topdir/.mypatches/$project
+         rm -rf $topdir/.mypatches/$project/*.patch
+         rm -rf $topdir/.mypatches/$project/*.diff
+
+         echo ">>>  project: $project ... "
          basebranch=$(git branch -a | grep '\->' | grep "$default_branch" | sed -e "s/.*\-> //")
          git format-patch "$basebranch" -o $(gettop)/.mypatches/$project/
+         patches_count=$(find $topdir/.mypatches/$project -name "*.patch" -o -name "*.diff" | wc -l)
+         [ $patches_count -eq 0 ] && rmdir -p --ignore-fail-on-non-empty $topdir/.mypatches/$project
+
+    done
+    mv $snap_file.new $snap_file
+    cd $topdir
+}
+
+function restore_snap()
+{
+    cd $(gettop)
+    topdir=$(gettop)
+    default_branch=$(cat .repo/manifest.xml | grep "default revision" | cut -d= -f2 | sed -e "s/\"//g" -e "s/refs\/heads\///")
+    snap_file=$topdir/.mypatches/projects_snap.list
+    [ -f "$snap_file" ] || return -1
+    cat $snap_file | while read line; do
+         project=$(echo $line | cut -d, -f1 | sed -e "s/^ *//g" -e "s/ *$//g")
+         basebranch=$(echo $line | cut -d, -f2 | sed -e "s/^ *//g" -e "s/ *$//g")
+         basecommit=$(echo $line | cut -d, -f3 | sed -e "s/^ *//g" -e "s/ *$//g")
+
+         cd $topdir/$project
+
+
+         echo ">>>  restore snapped project: $project ... "
+         git stash; git clean -xdf
+         git checkout --detach $basebranch
+         git reset --hard $basecommit
+
+         find .mypatches/$project -type f | sed -e "s/\.mypatches\///" |sort -n | while read f; do
+             patchfile=$(basename $f)
+             ext=${patchfile##*.}
+             rm -rf .git/rebase-apply
+             if [ "$ext" = "patch" -o "$ext" = "diff" ]; then
+                  changeid=$(grep "Change-Id: " $topdir/.mypatches/$f | tail -n 1 | sed -e "s/ \{1,\}/ /g" -e "s/^ //g" | cut -d' ' -f2)
+                  if [ "$changeid" != "" ]; then
+                      if ! git log  -100 | grep "Change-Id: $changeid" >/dev/null 2>/dev/null; then 
+                          echo "          apply patch: $f ..."
+                          git am -3 -q < $topdir/.mypatches/$f
+                          [ $? -ne 0 ] && exit -1
+                      else
+                          echo "          skip patch: $f ...(applied always)"
+                      fi
+                  fi
+             fi
+         done
+
     done
     cd $topdir
 }
 
-if [ $# -gt 1 ]; then
-   [ $save_patch -eq 1 ] && save_patches
-   [ $branch_reset -eq 1 ] && reset_branches
-   [ $patch_diff -eq 1 ] && patch_saved
+if [ $# -ge 1 ]; then
+   [ $op_snap_projects -eq 1 ] && projects_snap
+   [ $op_reset_projects -eq 1 ] && projects_reset
+   [ $op_patch_local -eq 1 ] && patch_local
+   [ $op_restore_snap -eq 1 ] && restore_snap
    exit 0
 fi
 
 ######################################
+#### repicks from haggertk #####
 
+function kpick() {
+  repopick $1 
+}
 
-# fw/base: Enable home button wake
-repopick 191580;
+#if false; then
 
-# hardware/qcom/audio
-repopick 196377;
+CAF_HALS="audio display media"
+for hal in $CAF_HALS; do
+  d=`pwd`
+  cd hardware/qcom/${hal}-caf/msm8974 || exit 1
+  git remote remove bgcngm 2>/dev/null
+  git remote add bgcngm https://github.com/bgcngm/android_hardware_qcom_${hal}.git || exit 1
+  git fetch bgcngm staging/lineage-15.1-caf-8974-rebase-LA.BF.1.1.3_rb1.15  || exit 1
+  git checkout bgcngm/staging/lineage-15.1-caf-8974-rebase-LA.BF.1.1.3_rb1.15 || exit 1
+  cd $d
+done
 
+kpick 199120 # tinycompress: HAXXX: Move libtinycompress_vendor back to Android.mk
+
+# device/lineage/sepolicy
+kpick 198594 # sepolicy: qcom: Import bluetooth_loader/hci_attach rules
+kpick 199347 # sepolicy: Set the context for fsck.exfat/ntfs to fsck_exec
+kpick 199348 # sepolicy: Add domain for mkfs binaries
+kpick 199349 # sepolicy: label exfat and ntfs mkfs executables
+kpick 199350 # sepolicy: treat fuseblk as sdcard_external
+kpick 199351 # sepolicy: fix denials for external storage
+kpick 199352 # sepolicy: Allow vold to `getattr` on mkfs_exec
+kpick 199353 # sepolicy: allow vold to mount fuse-based sdcard
+kpick 199515 # sepolicy: Add policy for sysinit
+kpick 199516 # sepolicy: allow userinit to set its property
+kpick 199517 # sepolicy: Permissions for userinit
+kpick 199518 # sepolicy: Fix sysinit denials
+kpick 199571 # sepolicy: Move fingerprint 2.0 service out of private sepolicy
+kpick 199572 # sepolicy: SELinux policy for persistent properties API
+
+# device/qcom/sepolicy
+kpick 198620 # sepolicy: Let keystore load firmware
+kpick 198703 # Revert "sepolicy: Allow platform app to find nfc service"
+kpick 198707 # sepolicy: Include legacy rild policies
+kpick 198141 # Use set_prop() macro for property sets
+kpick 198303 # sepolicy: Add sysfs labels for devices using 'soc.0'
+kpick 199557 # sepolicy: Readd perfd policies
+kpick 199558 # sepolicy: Allow system_app to connect to time_daemon socket
+kpick 199559 # sepolicy: Allow dataservice_app to read/write to IPA device
+kpick 199560 # sepolicy: Allow bluetooth to connect to wcnss_filter socket
+kpick 199562 # sepolicy: Allow netmgrd to communicate with netd
+kpick 199562 # sepolicy: Allow netmgrd to communicate with netd
+kpick 199564 # sepolicy: Allow energyawareness to read sysfs files
+kpick 199565 # sepolicy: Label pre-O location data and socket file paths
+kpick 199554 # sepolicy: Add /data/vendor/time label for old oreo blobs
+kpick 199600 # sepolicy: Allow 'sys_admin' capability for rmt_storage
+
+# hardware/broadcom/libbt
+kpick 200115 # libbt: Add btlock support
+kpick 200116 # libbt: Add prepatch support
+kpick 200117 # libbt: Add support for using two stop bits
+kpick 200118 # libbt-vendor: add support for samsung bluetooth
+kpick 200119 # libbt-vendor: Add support for Samsung wisol flavor
+kpick 200121 # libbt-vendor: Fix Samsung patchfile detection.
+kpick 200122 # Avoid an annoying bug that only hits BCM chips running at less than 3MBps
+kpick 200123 # libbt-vendor: add support for Samsung semco
+kpick 200124 # Broadcom BT: Add support fm/bt via v4l2.
+kpick 200126 # libbt: Import CID_PATH from samsung_macloader.h
+kpick 200127 # libbt: Only allow upio_start_stop_timer on 32bit arm
+
+# hardware/samsung
+kpick 200133 # macloader: Stop allowing G and O write perms to the cidfile
+
+# system/sepolicy
+kpick 199664 # sepolicy: Fix up exfat and ntfs support
+
+#fi
+
+########## more picks ################
+
+#exit 0
+
+######## repopicks from afaneh92 ##############
+
+repopick  198959 # PackageManager: Add configuration to specify vendor platform signatures
+repopick  198950 # Enable NSRM (Network Socket Request Manager).
+repopick  198951 # CamcorderProfiles: Add new camcorder profiles
+repopick  198952 # HAX: add LPCM to list
+#repopick  198953 # kernel: Handle kernel modules correctly
+repopick  198954 # build: Make systemimage depend on installed kernel if system is root
+repopick  198960 # update_verifier: skip verity to determine successful on lineage builds
+repopick  198955 # init: don't reboot to bootloader on panic
+repopick  198958 # init: I hate safety net
+
+repopick  198956 # envsetup: Update default path for SDCLANG 4.0
+repopick  198962 # Init: Support bootdevice symlink for early mount.
+repopick  198961 # frameworks/base: Support for third party NFC features and extensions
+
+repopick  198949 # kernel: don't build for TARGET_NO_KERNEL targets
+repopick  198957 # NFC: Adding new vendor specific interface to NFC Service
+#repopick  198036 # softap: sdk: Declare VNDK usage
+#repopick  198029 # bash: Disable clang
+repopick  198050 # nxp: NativeNfcManager: Implement missing inherited abstract methods
+repopick  198095 # sepolicy: Ignore custom lineage additions
+
+repopick  198967 # InputMethodManagerService: adjust grip mode for input enable/disable
+
+repopick 198593 # fw/base: Enable home button wake
+
+repopick 199357 # FMRadio: fix missing libfmjni and include liblog
+repopick 198902 # Remove include for dtbhtool
+#repopick 199143 # macloader: Move device dependent modules to /vendor
+#repopick 199144 # Revert "msm8x74: remove from top level makefile"
 # hardware/qcom/display
-repopick 196378 196379 196380 196381;
-
-# hardware/qcom/gps
-repopick 185675 # Revert "msm8974: deprecate msm8974"
-repopick 185676 # Revert "msm8974: remove from top level makefile"
-repopick 187702 # msm8974: Add missing liblog dependency
-
-# hardware/qcom/media
-repopick 185806 # mm-video: venc: Correct a typo in variable name
-
+repopick 199145 199146 199147
 # Topic: samsung-libril-oreo
-repopick -c 30 -Q 'status:open+topic:samsung-libril-oreo+branch:lineage-15.0'; ## https://review.lineageos.org/#/q/status:open+topic:samsung-libril-oreo
-
-# macloader: Move device dependent modules to /vendor
-repopick 195655;
-
-# Recovery updates
-repopick 187155; ## https://review.lineageos.org/#/c/187155 [WIP]
-
+repopick -c 30 -Q 'status:open+topic:samsung-libril-oreo+branch:staging/lineage-15.1'
 # Native and core updates
-repopick -f 185639 # Restore android_alarm.h kernel uapi header
-repopick 185671; ## https://review.lineageos.org/#/c/185671 [Review]
-repopick 185888 187146; ## https://review.lineageos.org/#/q/change:185888+OR+change:187146 [DNM]
-repopick 190614 # linker: allow the linker to shim executables
-
+repopick 198955;
 # Apps and UI updates
-repopick 188389 188518-188526; ## https://review.lineageos.org/#/q/project:LineageOS/android_packages_apps_Camera2+branch:lineage-15.0 [Review]
-repopick 188527-188529; ## https://review.lineageos.org/#/q/project:LineageOS/android_packages_apps_Gallery2+branch:lineage-15.0 [Review]
-
-# Camera HAL1
-repopick -Q 'status:open+topic:android-o-camera-hal1+branch:lineage-15.0'; ## https://review.lineageos.org/#/q/status:open+topic:android-o-camera-hal1+branch:lineage-15.0 [WIP]
-
+repopick -P packages/apps/Camera2 188389 188518-188526
+repopick -P packages/apps/Gallery2 188527-188529
+# WIP repopick -Q 'status:open+topic:android-o-mr1-camera-hal1';
 # LineageOS Additions
-repopick -f -c 30 197655
-repopick -c 30 -Q 'status:open+topic:oreo-powermenu+branch:lineage-15.0'; ## https://review.lineageos.org/#/q/status:open+topic:oreo-powermenu+branch:lineage-15.0 [ToFinish]
-repopick -c 30 193025 193758; ## https://review.lineageos.org/#/q/change:193025 [ToMerge]
-repopick -f -c 30 197765; ## https://review.lineageos.org/#/q/change:197765 [ToMerge]
-repopick -c 30 191736 193029-193031 193033 193026 193027 193032; ## https://review.lineageos.org/#/q/change:191736+OR+change:193029+OR+change:193030+OR+change:193031+OR+change:193033+OR+change:193026+OR+change:193027+OR+change:193032 [ToMerge]
-repopick -c 30 193758 193249 193258; ## https://review.lineageos.org/#/q/change:193758+OR+change:193249+OR+change:193258 [Review]
-repopick 193544 193770; ## https://review.lineageos.org/#/q/change:193544+OR+change:193770 [ToFinish]
+repopick -c 30 -Q 'status:open+topic:oreo-visualizer+branch:staging/lineage-15.1'
+repopick -c 30 -Q 'status:open+topic:oreo-tiles+branch:staging/lineage-15.1'
+repopick -c 30 -Q 'status:open+topic:oreo-network-traffic+branch:staging/lineage-15.1'
