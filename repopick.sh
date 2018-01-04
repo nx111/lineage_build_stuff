900fbe486a41afcc9f1b8703e9427baf6cbe9ea7
nx111 <gd.zhangdz@gmail.com>
2018-01-04 16:29:18 +0800

    repoick.sh: update changes.

--- repopick.sh
+++ repopick.sh
@@ -201,10 +201,6 @@ fi
 ######################################
 #### repicks from haggertk #####
 
-function kpick() {
-  repopick $1 
-}
-
 :<<_COMMENT_
 CAF_HALS="audio display media"
 for hal in $CAF_HALS; do
@@ -218,59 +214,59 @@ for hal in $CAF_HALS; do
 done
 _COMMENT_
 
-kpick 199120 # tinycompress: HAXXX: Move libtinycompress_vendor back to Android.mk
+repopick 199120 # tinycompress: HAXXX: Move libtinycompress_vendor back to Android.mk
 
 # device/lineage/sepolicy
-kpick 198594 # sepolicy: qcom: Import bluetooth_loader/hci_attach rules
-kpick 199347 # sepolicy: Set the context for fsck.exfat/ntfs to fsck_exec
-kpick 199348 # sepolicy: Add domain for mkfs binaries
-kpick 199349 # sepolicy: label exfat and ntfs mkfs executables
-kpick 199350 # sepolicy: treat fuseblk as sdcard_external
-kpick 199351 # sepolicy: fix denials for external storage
-kpick 199352 # sepolicy: Allow vold to `getattr` on mkfs_exec
-kpick 199353 # sepolicy: allow vold to mount fuse-based sdcard
-kpick 199515 # sepolicy: Add policy for sysinit
-kpick 199516 # sepolicy: allow userinit to set its property
-kpick 199517 # sepolicy: Permissions for userinit
-kpick 199518 # sepolicy: Fix sysinit denials
-kpick 199571 # sepolicy: Move fingerprint 2.0 service out of private sepolicy
-kpick 199572 # sepolicy: SELinux policy for persistent properties API
+repopick 198594 # sepolicy: qcom: Import bluetooth_loader/hci_attach rules
+repopick 199347 # sepolicy: Set the context for fsck.exfat/ntfs to fsck_exec
+repopick 199348 # sepolicy: Add domain for mkfs binaries
+repopick 199349 # sepolicy: label exfat and ntfs mkfs executables
+repopick 199350 # sepolicy: treat fuseblk as sdcard_external
+repopick 199351 # sepolicy: fix denials for external storage
+repopick 199352 # sepolicy: Allow vold to `getattr` on mkfs_exec
+repopick 199353 # sepolicy: allow vold to mount fuse-based sdcard
+repopick 199515 # sepolicy: Add policy for sysinit
+repopick 199516 # sepolicy: allow userinit to set its property
+repopick 199517 # sepolicy: Permissions for userinit
+repopick 199518 # sepolicy: Fix sysinit denials
+repopick 199571 # sepolicy: Move fingerprint 2.0 service out of private sepolicy
+repopick 199572 # sepolicy: SELinux policy for persistent properties API
 
 # device/qcom/sepolicy
-kpick 198620 # sepolicy: Let keystore load firmware
-kpick 198703 # Revert "sepolicy: Allow platform app to find nfc service"
-kpick 198707 # sepolicy: Include legacy rild policies
-kpick 198141 # Use set_prop() macro for property sets
-kpick 198303 # sepolicy: Add sysfs labels for devices using 'soc.0'
-kpick 199557 # sepolicy: Readd perfd policies
-kpick 199558 # sepolicy: Allow system_app to connect to time_daemon socket
-kpick 199559 # sepolicy: Allow dataservice_app to read/write to IPA device
-kpick 199560 # sepolicy: Allow bluetooth to connect to wcnss_filter socket
-kpick 199562 # sepolicy: Allow netmgrd to communicate with netd
-kpick 199562 # sepolicy: Allow netmgrd to communicate with netd
-kpick 199564 # sepolicy: Allow energyawareness to read sysfs files
-kpick 199565 # sepolicy: Label pre-O location data and socket file paths
-kpick 199554 # sepolicy: Add /data/vendor/time label for old oreo blobs
-kpick 199600 # sepolicy: Allow 'sys_admin' capability for rmt_storage
+repopick 198620 # sepolicy: Let keystore load firmware
+repopick 198703 # Revert "sepolicy: Allow platform app to find nfc service"
+repopick 198707 # sepolicy: Include legacy rild policies
+repopick 198141 # Use set_prop() macro for property sets
+repopick 198303 # sepolicy: Add sysfs labels for devices using 'soc.0'
+repopick 199557 # sepolicy: Readd perfd policies
+repopick 199558 # sepolicy: Allow system_app to connect to time_daemon socket
+repopick 199559 # sepolicy: Allow dataservice_app to read/write to IPA device
+repopick 199560 # sepolicy: Allow bluetooth to connect to wcnss_filter socket
+repopick 199562 # sepolicy: Allow netmgrd to communicate with netd
+repopick 199562 # sepolicy: Allow netmgrd to communicate with netd
+repopick 199564 # sepolicy: Allow energyawareness to read sysfs files
+repopick 199565 # sepolicy: Label pre-O location data and socket file paths
+repopick 199554 # sepolicy: Add /data/vendor/time label for old oreo blobs
+repopick 199600 # sepolicy: Allow 'sys_admin' capability for rmt_storage
 
 # hardware/broadcom/libbt
-kpick 200115 # libbt: Add btlock support
-kpick 200116 # libbt: Add prepatch support
-kpick 200117 # libbt: Add support for using two stop bits
-kpick 200118 # libbt-vendor: add support for samsung bluetooth
-kpick 200119 # libbt-vendor: Add support for Samsung wisol flavor
-kpick 200121 # libbt-vendor: Fix Samsung patchfile detection.
-kpick 200122 # Avoid an annoying bug that only hits BCM chips running at less than 3MBps
-kpick 200123 # libbt-vendor: add support for Samsung semco
-kpick 200124 # Broadcom BT: Add support fm/bt via v4l2.
-kpick 200126 # libbt: Import CID_PATH from samsung_macloader.h
-kpick 200127 # libbt: Only allow upio_start_stop_timer on 32bit arm
+repopick 200115 # libbt: Add btlock support
+repopick 200116 # libbt: Add prepatch support
+repopick 200117 # libbt: Add support for using two stop bits
+repopick 200118 # libbt-vendor: add support for samsung bluetooth
+repopick 200119 # libbt-vendor: Add support for Samsung wisol flavor
+repopick 200121 # libbt-vendor: Fix Samsung patchfile detection.
+repopick 200122 # Avoid an annoying bug that only hits BCM chips running at less than 3MBps
+repopick 200123 # libbt-vendor: add support for Samsung semco
+repopick 200124 # Broadcom BT: Add support fm/bt via v4l2.
+repopick 200126 # libbt: Import CID_PATH from samsung_macloader.h
+repopick 200127 # libbt: Only allow upio_start_stop_timer on 32bit arm
 
 # hardware/samsung
-kpick 200133 # macloader: Stop allowing G and O write perms to the cidfile
+repopick 200133 # macloader: Stop allowing G and O write perms to the cidfile
 
 # system/sepolicy
-kpick 199664 # sepolicy: Fix up exfat and ntfs support
+repopick 199664 # sepolicy: Fix up exfat and ntfs support
 
 ########## more picks ################
 
@@ -278,46 +274,73 @@ kpick 199664 # sepolicy: Fix up exfat and ntfs support
 
 ######## repopicks from afaneh92 ##############
 
-repopick  198959 # PackageManager: Add configuration to specify vendor platform signatures
-repopick  198950 # Enable NSRM (Network Socket Request Manager).
-repopick  198951 # CamcorderProfiles: Add new camcorder profiles
-repopick  198952 # HAX: add LPCM to list
-#repopick  198953 # kernel: Handle kernel modules correctly
-repopick  198954 # build: Make systemimage depend on installed kernel if system is root
-repopick  198960 # update_verifier: skip verity to determine successful on lineage builds
-repopick  198955 # init: don't reboot to bootloader on panic
-repopick  198958 # init: I hate safety net
+repopick 198544 # SystemUI: Add visualizer feature
+repopick 198556 # Settings: Add lockscreen visualizer toggle
 
-repopick  198956 # envsetup: Update default path for SDCLANG 4.0
-repopick  198962 # Init: Support bootdevice symlink for early mount.
-repopick  198961 # frameworks/base: Support for third party NFC features and extensions
+repopick 198545 # base: Disable Lockscreen Media Art [1/3]
+repopick 198557 # Settings: Disable Lockscreen Media Art [2/3]
 
-repopick  198949 # kernel: don't build for TARGET_NO_KERNEL targets
-repopick  198957 # NFC: Adding new vendor specific interface to NFC Service
-#repopick  198036 # softap: sdk: Declare VNDK usage
-#repopick  198029 # bash: Disable clang
-repopick  198050 # nxp: NativeNfcManager: Implement missing inherited abstract methods
-repopick  198095 # sepolicy: Ignore custom lineage additions
+repopick 198546 # SystemUI: enable NFC tile
+repopick 198547 # SystemUI: add caffeine qs tile
+repopick 198548 # SystemUI: Add heads up tile
+repopick 198549 # QS: add Sync tile
+repopick 198550 # Added show volume panel tile to QS
+repopick 198551 # SystemUI: Add adb over network tile
+repopick 198552 # SystemUI: Readd AmbientDisplayTile.
+repopick 198553 # SystemUI: add USB Tether tile
 
-repopick  198967 # InputMethodManagerService: adjust grip mode for input enable/disable
+repopick 198554 # SystemUI: Network Traffic [1/3]
+repopick 198558 # lineage-sdk: Add Network Traffic [2/3]
+repopick 198559 # LineageParts: Network Traffic [3/3]
 
-repopick 198593 # fw/base: Enable home button wake
+#repopick 198622 # Add back increasing ring feature (2/3)
+#repopick 198624 # Add back increasing ring feature (3/3)
 
-repopick 199357 # FMRadio: fix missing libfmjni and include liblog
 repopick 198902 # Remove include for dtbhtool
-#repopick 199143 # macloader: Move device dependent modules to /vendor
-#repopick 199144 # Revert "msm8x74: remove from top level makefile"
-# hardware/qcom/display
-repopick 199145 199146 199147
-# Topic: samsung-libril-oreo
-repopick -c 30 -Q 'status:open+topic:samsung-libril-oreo+branch:staging/lineage-15.1'
-# Native and core updates
-repopick 198955;
-# Apps and UI updates
-repopick -P packages/apps/Camera2 188389 188518-188526
-repopick -P packages/apps/Gallery2 188527-188529
-# WIP repopick -Q 'status:open+topic:android-o-mr1-camera-hal1';
-# LineageOS Additions
-repopick -c 30 -Q 'status:open+topic:oreo-visualizer+branch:staging/lineage-15.1'
-repopick -c 30 -Q 'status:open+topic:oreo-tiles+branch:staging/lineage-15.1'
-repopick -c 30 -Q 'status:open+topic:oreo-network-traffic+branch:staging/lineage-15.1'
+
+repopick 200153 # StatusBar: Add dark theme toggle
+repopick 200154 # LineageSettings: Add dark theme toggle
+repopick 200155 # Settings: Add toggle for dark theme
+
+repopick 200031 # AudioFX: Apply Lineage SDK rebrand
+repopick 200032 # audiopolicy: Add AudioSessionInfo API
+repopick 200044 # AudioFX: Remove cyngn remnants
+repopick 200045 # AudioFX: rebrand step 1: update paths
+repopick 200046 # AudioFX: rebrand step 2: update file contents
+repopick 200047 # Revert "cm: include CMAudioService in builds"
+repopick 200078 # Revert "cmsdk: Broker out CMAudioService"
+repopick 200033 # lineage: Reenable AudioFX and remove LineageAudioService
+
+#repopick 198956 # envsetup: Update default path for SDCLANG 4.0
+#repopick 200167 # Add support for building with proprietary compiler
+#repopick 200168 # Control building shared libs, static libs and executables with SDLLVM LTO
+#repopick 200169 # Add support for using the secondary SDLLVM toolchain
+#repopick 200170 # Turn off sdclang for cfi sanitizer
+#repopick 200171 # build: Require devices to opt-in for SDCLANG
+#repopick 200172 # binary: Append cc/cxx wrapper to sdclang
+#repopick 200173 # dumpvar: Dump TARGET_USE_SDCLANG
+
+repopick 200308 # init: always allow local.prop overrides
+repopick 198959 # PackageManager: Add configuration to specify vendor platform signatures
+repopick 198950 # Enable NSRM (Network Socket Request Manager).
+repopick 198951 # CamcorderProfiles: Add new camcorder profiles
+repopick 198952 # HAX: add LPCM to list
+repopick 198953 # kernel: Handle kernel modules correctly
+repopick 198954 # build: Make systemimage depend on installed kernel if system is root
+repopick 198960 # update_verifier: skip verity to determine successful on lineage builds
+repopick 198955 # init: don't reboot to bootloader on panic
+repopick 198958 # init: I hate safety net
+
+repopick 198962 # Init: Support bootdevice symlink for early mount.
+repopick 198961 # frameworks/base: Support for third party NFC features and extensions
+
+repopick 198949 # kernel: don't build for TARGET_NO_KERNEL targets
+repopick 198957 # NFC: Adding new vendor specific interface to NFC Service
+repopick 198050 # nxp: NativeNfcManager: Implement missing inherited abstract methods
+repopick 198967 # InputMethodManagerService: adjust grip mode for input enable/disable
+
+#repopick 198109 # lineagehw: Use color matricies for HWC2 color calibration
+#repopick 198110 # sdk: Add DisplayUtils for global display matrix setting
+#repopick 198111 # livedisplay: Use new DisplayTransformManager API to set color overlay
+
+
