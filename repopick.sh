a181e5c57645cbe74fcb4c26aa3abafad5fc4093
nx111 <gd.zhangdz@gmail.com>
2018-01-22 16:10:26 +0800

    snapshot: 20180122-1146

--- repopick.sh
+++ repopick.sh
@@ -346,6 +346,7 @@ function kpick()
 
 # build/make
 kpick 202441 # core: config: Add inline kernel headers to AOSP kernel header path
+kpick 203381 # Revert "build: don't dex preopt by default on linux"
 
 # device/samsung/klte-common
 kpick 203304 # klte-common: power: Add legacy qcom HAL compat code
@@ -358,56 +359,29 @@ kpick 203303 # Revert "msm8974-common: Use QTI HIDL power HAL"
 
 # device/lineage/sepolicy
 kpick 198594 # sepolicy: qcom: Import bluetooth_loader/hci_attach rules
-kpick 199347 # sepolicy: Set the context for fsck.exfat/ntfs to fsck_exec
-kpick 199348 # sepolicy: Add domain for mkfs binaries
-kpick 199349 # sepolicy: label exfat and ntfs mkfs executables
-kpick 199350 # sepolicy: treat fuseblk as sdcard_external
-kpick 199351 # sepolicy: fix denials for external storage
-kpick 199352 # sepolicy: Allow vold to `getattr` on mkfs_exec
-kpick 199353 # sepolicy: allow vold to mount fuse-based sdcard
-kpick 199515 # sepolicy: Add policy for sysinit
-kpick 199516 # sepolicy: allow userinit to set its property
-kpick 199517 # sepolicy: Permissions for userinit
-kpick 199518 # sepolicy: Fix sysinit denials
 kpick 199572 # sepolicy: SELinux policy for persistent properties API
 kpick 201552 # Squashed import of superuser SELinux policies
 kpick 201582 # sepolicy: adapt sudaemon policy for O
 kpick 203433 # sepolicy: Allow apps with API level <= 25 to access services
+kpick 203558 #sepolicy: Add policy for vendor.lineage.power HAL
 
 # device/qcom/common
 kpick 201274 # power: Update power hal extension for new qti hal
 
 # device/qcom/sepolicy
-kpick 198141 # Use set_prop() macro for property sets
-kpick 199557 # legacy: add back perfd sepolicy
 kpick 199559 # sepolicy: Allow dataservice_app to read/write to IPA device
-kpick 202379 # legacy: add back radio rules
-kpick 202381 # legacy: add back rules for non-treble devices
-kpick 202382 # legacy: allow rmt_storage sys_admin capability
-kpick 202383 # legacy: let rfs_access do msm ipc ioctls
-kpick 202384 # legacy: label old hardcoded data paths
-kpick 202385 # legacy: label old msm_irqbalance prop
-kpick 202386 # legacy: add back ipacm rules
-kpick 202387 # legacy: add back imscm support into ims
-kpick 202388 # legacy: allow rild to access radio data files
-kpick 202389 # legacy: Fix labeling the thermal sockets
-kpick 202390 # legacy: let audioserver connect to thermal engine sockets
-kpick 202391 # legacy: label per_mgr as a binder service
-kpick 198303 # sepolicy: Add sysfs labels for devices using 'soc.0'
-kpick 202824 # legacy: Readd support for old perfd socket
-kpick 202825 # legacy: Add back old fdAlbum rule
-kpick 202826 # sepolicy: Label boot/recovery/cache/system partitions
-kpick 202827 # legacy: Label old UIO sysfs
-kpick 202828 # legacy: Allow qmuxd access diag
-kpick 202829 # legacy: Label old SSR sysfs
 kpick 202830 # legacy: Add back legacy sensors rules
-kpick 202831 # legacy: Label old kgsl sysfs nodes
 kpick 202995 # legacy: Address mpdecision denials
 
 # frameworks/av
 kpick 198113 # camera/media: Support for legacy camera HALv1
 kpick 198114 # libstagefright: Support for legacy camera/encoder buffers
 kpick 198116 # CameraService: Fix deadlock in binder death cleanup.
+kpick 201731 # libstagefright: use 64-bit usage for native_window_set_usage
+kpick 203520 # media: fix infinite wait at source for HAL1 based recording
+kpick 203521 # libstagefright: Free buffers on observer died
+kpick 203522 # stagefright: ACodec: Resolve empty vendor parameters usage
+kpick 203523 # Camera: fix use after disconnect error
 
 # frameworks/base
 kpick 198701 # AppOps: track op persistence by name instead of id
@@ -415,9 +389,10 @@ kpick 200969 # SystemUI: Power menu customizations
 kpick 201879 # frameworks: Privacy Guard for O
 kpick 199947 # PowerManager: Re-integrate button brightness
 kpick 202423 # Screenshot: append app name to filename
+kpick 202874 # Show infinity for large notification counts
 kpick 203053 # perf: Add plumbing for PerformanceManager
 kpick 203054 # perf: Adapt for HIDL Lineage power hal
-kpick 202701 # SEEMP: framework instrumentation and AppProtect features
+#kpick 202701 # SEEMP: framework instrumentation and AppProtect features
 kpick 203441 # Fix double action performed when pressing menu key
 
 # frameworks/native
@@ -436,13 +411,11 @@ kpick 203061 # lineage/interfaces: power: Add binderized service
 
 # hardware/qcom/power
 kpick 203055 # power: Prepare for power profile support
-kpick 203064 # power: Remove mutex to camera hints
 kpick 203066 # power: Add known perf hint IDs
 kpick 203067 # power: msm8996: Add support for power profile and cpu boost
 kpick 203115 # power: Enable interaction boost unconditionally
 
 # lineage-sdk
-kpick 200970 # lineage-sdk: Import power menu related classes
 kpick 203030 # lineage-sdk: Add overlay support for disabling hardware features
 kpick 203011 # lineage-sdk: Reenable performance profiles
 
@@ -454,24 +427,17 @@ kpick 201634 # Allow using private framework API.
 #kpick 200069 # LineageParts: Deprecate few button settings
 #kpick 199198 # LineageParts: Bring up buttons settings
 kpick 199948 # LineageParts: Bring up button backlight settings
-kpick 201309 # LineageParts: Re-enable PowerMenuActions and adapt to SDK updates
 kpick 201528 # PrivacyGuard: Bring up and inject into Settings
 kpick 203010 # LineageParts: enable perf profiles
 kpick 203440 # LineageParts: Match default menu long press action with actual config
+kpick 203550 # LineageParts: Fix up additional styles for O
 
 # packages/apps/Settings
-kpick 199839 # Settings: Add advanced restart switch
 kpick 201529 # Settings: Privacy Guard
 kpick 201531 # Settings: Add developer setting for root access
 kpick 203009 # Settings: battery: Add LineageParts perf profiles
 
 # packages/apps/Snap
-kpick 202561 # QuickReader: Match switch icon size and fill color with other icons
-kpick 202562 # Camera: Cleanup hardware key handling
-kpick 202563 # Camera: Handle keys only while in app
-kpick 202564 # Camera2: Headset shutter mode
-kpick 202565 # Camera2: Add option to set max screen brightness
-kpick 202566 # Snap: Make developer menu more accessible
 kpick 202685 # Snap: Rebrand to org.lineageos.snap
 kpick 203342 # Snap: update shutter buttons on CaptureUI
 
@@ -485,8 +451,7 @@ kpick 202596 # fs_config: fix fileperms for su-binary
 # system/sepolicy
 kpick 198106 # Add rules required for TARGET_HAS_LEGACY_CAMERA_HAL1
 kpick 198107 # Adapt add_service uses for TARGET_HAS_LEGACY_CAMERA_HAL1
-kpick 198108 # mediaserver: Allow finding the hal_camera hardware service
-kpick 199664 # sepolicy: Fix up exfat and ntfs support
+#kpick 198108 # mediaserver: Allow finding the hal_camera hardware service
 kpick 201553 # sepolicy: We need to declare before referencing
 kpick 201583 # sepolicy: Allow su by apps on userdebug_or_eng
 kpick 201584 # sepolicy: update policies for sudaemon on O
@@ -495,7 +460,7 @@ kpick 203330 # treble sepolicy tests: Workaround /sbin services labels
 
 #vendor/lineage
 kpick 201336 # soong_config: Add TARGET_HAS_LEGACY_CAMERA_HAL1 variable
-kpick 202692 # lineage: Enable boot and system server dex-preopt
+#kpick 202692 # lineage: Enable boot and system server dex-preopt
 
 ##################################
 
